e7965fd8310e1ffa9bab6f326041cb01c8ce229e
Frizz Khalifa <akw34@txstate.edu>
2018-03-05 19:46:00 -0600

    add project 2

--- Project2/js/main.js
+++ Project2/js/main.js
@@ -1,14 +1,19 @@
-/**
+/*
 1. Get value for gender radio button
 2. Loop through dog rows, check if row is equal to value of radio button
 3. If so, display dog, if not, hide dog.
-**/
+*/
 
-var gender;
-var age;
+var selectedGender;
+var selectedAge;
+
+var allDogs = document.querySelectorAll(".dog");
 
 //Write function for gender radio buttons
 var processDogForm = function(event) {
+
+ //Gender Button function
+ //Find elements with name = gender, call them genderButtons
   var genderButtons = document.getElementsByName('gender');
 	for (var i = 0; i < genderButtons.length; i++) {
 			if (genderButtons[i].checked) {
@@ -17,23 +22,49 @@ var processDogForm = function(event) {
 				break; // quits the for loop once you find the option that's selected
 			}
 	}
-  console.log(selectedGender);
 
-var allDogs = document.querySelectorAll(".dog");
+  //Age Button function
+  //Find elements with name=age, call them ageButtons
+  var ageButtons = document.getElementsByName('age');
+  for (var i = 0; i < ageButtons.length; i++) {
+      if (ageButtons[i].checked) {
+        // Get value for gender buttons selected
+        selectedAge = ageButtons[i].value;
+        break; // quits the for loop once you find the option that's selected
+      }
+  }
+
   for (var i = 0; i < allDogs.length; i++) {
     // Check if dogs gender matches gender selected
     var currentGender = allDogs[i].getAttribute('data-gender');
-    if (selectedGender == currentGender) {
-    //Show current dog that matches selected gender
-      document.getAttribute('data-gender').style.display = 'block');
+    var currentAge = allDogs[i].getAttribute('data-age');
+    //Add && to this to trigger age
+
+      if (selectedGender == undefined) {
+        if (selectedAge == currentAge) {
+          allDogs[i].setAttribute('class', 'col-md-4 visible');
+        } //if selectedAge == currentAge
+      } //if selectedGender == undefined
+    else if (selectedAge == undefined) {
+      if (selectedGender == currentGender) {
+        allDogs[i].setAttribute('class', 'col-md-4 visible');
+      } // selectedGender == currentGender
+    } //selectedAge == undefined
+
+    else if (selectedGender == currentGender && selectedAge == currentAge) {
+      //Show current dog that matches selected gender & age
+      allDogs[i].setAttribute('class', 'col-md-4 visible');
   } else {
       //If not, hide it
-      document.getAttribute('data-gender').style.display = 'none');
+      allDogs[i].setAttribute('class', 'hidden');
     }
+
   }
 
+  event.preventDefault();
+
+}
 
 //Find form:
 var form = document.getElementById('infoForm');
-
-form.addEventListener('change', processDogForm , false)
+form.addEventListener('change', processDogForm , false);
